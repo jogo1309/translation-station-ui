a882803f327c18ff00a9dbf32fe2d014d2b39e04
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2021-01-25 12:11:04 +0000

    inital commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import "./App.css";
+import { CssBaseline } from "@material-ui/core";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home/home";
+
+export default function App() {
+  console.log(process.env.REACT_APP_API_ENDPOINT);
+  return (
+    <div>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
---
+++ src/Home/home.jsx
@@ -0,0 +1,12 @@
+import { Container, Paper, Typography } from "@material-ui/core";
+import React from "react";
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Paper>
+        <Typography>test</Typography>
+      </Paper>
+    </Container>
+  );
+}
