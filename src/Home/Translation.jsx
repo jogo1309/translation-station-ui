c1c5cdd3ed53ddcfcd79c49668b14b049414faf4
joshua.wood@btinternet.com <joshua.wood@btinternet.com>
2021-01-26 15:03:57 +0000

    added translation icon to button

--- src/Home/Translation.jsx
+++ src/Home/Translation.jsx
@@ -1,6 +1,7 @@
 import { TextField, Grid, Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
+import TranslateIcon from "@material-ui/icons/Translate";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,7 +23,7 @@ export default function Translation(props) {
       </Grid>
 
       <Grid item xs={2} classes={{ root: classes.root }}>
-        <Button color="primary" variant="contained">
+        <Button color="primary" variant="contained" endIcon={<TranslateIcon />}>
           Translate
         </Button>
       </Grid>
